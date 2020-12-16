a045c6b01a8bfef9d7d6986ababa7db8db0df83e
victory studio <hello@victory.studio>
2020-12-17 00:21:10 +0200

    добавил синусоиду для анимации звука

--- gulpfile.js
+++ gulpfile.js
@@ -86,6 +86,7 @@ var path = {
           'src/**/js/**/offscroll.js',
           'src/**/js/**/three.js',
           // 'src/**/js/**/checkbox.js',
+          'src/**/js/**/sinewave.js',
           // 'src/**/js/**/btn.js',
           'src/**/js/**/scrollanimation.js',
         ],
